98f2e3e82396d4f9718e2b83c288502b21f7e278
Lucas Braga Santos <lucasbrasantos@gmail.com>
2025-02-21 00:33:11 -0300

    feat: Implement CRUD operations for Person (master)
    This commit implements CRUD (Create, Read, Update, Delete)
    operations for the Person entity.  It includes API
    endpoints for creating, retrieving, updating, and
    deleting person records.  A new PersonRequest record
    is introduced to handle incoming requests.  The
    PersonModel is updated to support name updates and
    inactivation.

--- Models/PersonModel.cs
+++ Models/PersonModel.cs
@@ -3,11 +3,21 @@
 public class PersonModel
 {
     public Guid Id { get; init; }
-    public string Name { get; set; }
+    public string Name { get; private set; }
 
     public PersonModel(string name)
     {
         Id = Guid.NewGuid();
         Name = name;
+    }
+
+    public void UpdateName(string name)
+    {
+        Name = name;
+    }
+
+    public void SetInactive()
+    {
+        Name = "desativado";
     }
 }
--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -1,4 +1,6 @@
-﻿using Person.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using Person.Data;
+using Person.Models;
 
 namespace Person.Routes;
 
@@ -7,10 +9,49 @@ public static class PersonRoute
 
     public static void PersonRoutes(this WebApplication app)
     {
-        app.MapGet("Person", () =>
+        var route = app.MapGroup("person");
+        
+        route.MapGet("", async (PersonContext context) =>
         {
-            PersonModel pessoa = new PersonModel("Lucas");
-            return pessoa;
+            var people = await context.People.ToListAsync();
+            return Results.Ok(people);
+        });
+
+        route.MapPost("", async (PersonRequest req, PersonContext context) =>
+        {
+            var person = new PersonModel(req.name);
+            await context.AddAsync(person);
+            await context.SaveChangesAsync(); // "commit" ira salvar os dados no banco de dados
+        });
+
+        route.MapPut("{id:guid}", async (Guid id, PersonRequest req, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (person == null)
+            {
+                return Results.NotFound();
+            }
+            
+            person.UpdateName(req.name);
+            await context.SaveChangesAsync();
+            return Results.Ok(person);
+            
+        });
+
+        route.MapDelete("{id:guid}", async (Guid id, PersonContext context) =>
+        {
+            var person = await context.People.FirstOrDefaultAsync(x => x.Id == id);
+            
+            if (person == null)
+            {
+                return Results.NotFound();
+            }
+            
+            person.SetInactive();
+            await context.SaveChangesAsync();
+            return Results.Ok(person);
+
         });
     }
     
